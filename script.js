907603dbd7b27152b7768855086566772112a81e
Giulia Stella <ggiulia757@gmail.com>
2026-04-24 21:55:12 -0300

    animacoes

--- script.js
+++ script.js
@@ -11,31 +11,42 @@ const total = items.length;
 let timer;
 
 
-function update(direction){
+function update(direction) {
     document.querySelector('.item.active').classList.remove('active')
     document.querySelector('.dot.active').classList.remove('active')
 
-    if(direction > 0){
+    if (direction > 0) {
         active = active + 1
 
-        if (active === total){
+        if (active === total) {
             active = 0
         }
 
-    } else if (direction < 0){
+    } else if (direction < 0) {
+        active = active - 1
+        if (active < 0) {
+            active = total - 1
+        }
 
     }
-
-
-
     items[active].classList.add('active')
     dots[active].classList.add('active')
 
-}
+    nummberIndicator.textContent = String(active + 1).padStart(2, '0')
 
-prevButton.addEventListener('click', function(){
+}
+clearInterval(timer)
+timer = setInterval(() => {
+    update(1)
+}, 5000)
+
+
+
+
+
+prevButton.addEventListener('click', function () {
     update(-1)
 })
-nextButton.addEventListener('click', function(){
+nextButton.addEventListener('click', function () {
     update(1)
 })
